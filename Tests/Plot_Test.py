ad523c04850e075093ddc7aed8e1c1187e19d5c5
Vinyas Bhat <vinyas2020@gmail.com>
2024-03-23 18:06:44 -0700

    playing with live plotting...

--- Tests/Plot_Test.py
+++ Tests/Plot_Test.py
@@ -1,9 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from time import sleep
+
+import matplotlib.animation as animation
 
 x = np.linspace(0, 2 * np.pi, 200)
 y = np.sin(x)
 
+plt.ion()
 fig, ax = plt.subplots()
 ax.plot(x, y)
-plt.show()
+plt.show(block=True)
+
+sleep(2)
+y = y**2
+ax.plot(x,y)
+
