5c766a81c50427d390b5faabf2524dd8b0b2bfa7
Vinyas Bhat <vinyas2020@gmail.com>
2024-03-29 21:36:06 -0700

    finally got some live plotting working. Nexts steps: maybe optimize a bit, or just go ahead and combine it with the serial comm code to plot from the sensor.

--- Tests/LivePlot_Test.py
+++ Tests/LivePlot_Test.py
@@ -2,22 +2,23 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
 from random import randint
+import numpy as np
 
 style.use('fivethirtyeight')
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
-global x 
 x = [0]
 y = [0]
+x_interval = 0.1
 
 
 def animate(i):
-    x += x[len(x)-1] + 1
-    y += randint(0,10)
+    x.append(x[len(x)-1] + x_interval)
+    y.append(np.sin(x[len(x)-1]))
     ax1.clear
-    ax1.plot(x,y)
+    ax1.plot(x,y, color='black')
 
-ani = animation.FuncAnimation(fig, animate, interval=1000)
+ani = animation.FuncAnimation(fig, animate, interval=1)
 plt.show()
 
